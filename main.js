45bec0c4f155aed5a0959a6e54d639d97e5539fd
AniAvazneli <avazneli.ani@gmail.com>
2022-07-29 11:05:20 +0400

    created variables to catch html elements

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const chosenX = document.getElementById("cheseXcubeId");
+const chesenO = document.getElementById("cheseOcubeId");
+const vsCPU = document.getElementsByClassName("newGameCPU")[0];
+const vsPlayer = document.getElementsByClassName("newGamePlayer")[0];
