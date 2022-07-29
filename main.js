57292c1b7d76e4410167319a5f9ed3e61ab6dc2f
AniAvazneli <avazneli.ani@gmail.com>
2022-07-29 15:22:55 +0400

    before hovers

--- main.js
+++ main.js
@@ -4,10 +4,60 @@ const chosenX = document.getElementById("cheseXcubeId");
 const chesenO = document.getElementById("cheseOcubeId");
 const vsCPU = document.getElementsByClassName("newGameCPU")[0];
 const vsPlayer = document.getElementsByClassName("newGamePlayer")[0];
+const  SecStartGame = document.getElementsByClassName("startGame")[0];
+
+
+//variables for chosen buttons
+let activButton ;
+let currentPlayer ;
+let xScores = document.getElementsByClassName('XandChosenNumber')[0];
+let tiesText = document.getElementsByClassName('tiesText')[0].innerHTML = 'TIES';
+let oScores = document.getElementsByClassName('oandChosenNumber')[0];
 
 
 // catch clicks and make moves
-chosenX.addEventListener('click');
-chesenO.addEventListener('click');
-vsCPU.addEventListener('click');
+chosenX.addEventListener('click', palyerIsX);
+chesenO.addEventListener('click', palyerIsO);
+vsCPU.addEventListener('click', playerChosesCPU);
 vsPlayer.addEventListener('click');
+
+// functions for clicks :
+
+// 1. if person choses X mark
+function palyerIsX(){
+    currentPlayer = 'X';
+    chosenX.style.backgroundColor = "#A8BFC9";
+    document.getElementsByClassName('notChoseX')[0].style.display = "none";
+    document.getElementsByClassName('ChoseX')[0].style.display = "flex";
+    chesenO.style.backgroundColor = "#1A2A33";
+    document.getElementsByClassName('choseO')[0].style.display = "none";
+    document.getElementsByClassName('notChoseO')[0].style.display = "flex";
+}
+
+// 2. if person choses O mark
+function palyerIsO(){
+    currentPlayer = 'O';
+    chesenO.style.backgroundColor = "#A8BFC9";
+    document.getElementsByClassName('notChoseO')[0].style.display = "none";
+    document.getElementsByClassName('choseO')[0].style.display = "flex";
+    chosenX.style.backgroundColor = "#1A2A33";
+    document.getElementsByClassName('ChoseX')[0].style.display = "none";
+    document.getElementsByClassName('notChoseX')[0].style.display = "flex";
+}
+
+// 3. player choses CPU 
+function playerChosesCPU(){
+    if(currentPlayer === 'X'){
+    document.getElementsByClassName('newGame')[0].style.display = "none";
+    SecStartGame.style.display = "flex";
+    document.getElementsByClassName('turnImageX')[0].style.display = "flex";
+    document.getElementsByClassName('XandChosen')[0].innerHTML = 'X (YOU)';
+    xScores.innerHTML = '0';
+    tiesText;
+    document.getElementsByClassName('tiesNumber')[0].innerHTML = '0';
+    document.getElementsByClassName('oandChosen')[0].innerHTML = 'O (CPU)';
+    oScores.innerHTML = '0';
+    }
+}
+
+//
