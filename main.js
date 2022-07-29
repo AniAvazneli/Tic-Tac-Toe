1d6445c9972cced1b42cfa456921d4fa6349e8a3
AniAvazneli <avazneli.ani@gmail.com>
2022-07-29 12:26:47 +0400

    changed default visual of choosing X or O

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 'use strict';
-
+// catch html elements
 const chosenX = document.getElementById("cheseXcubeId");
 const chesenO = document.getElementById("cheseOcubeId");
 const vsCPU = document.getElementsByClassName("newGameCPU")[0];
 const vsPlayer = document.getElementsByClassName("newGamePlayer")[0];
+
+
+// catch clicks and make moves
+chosenX.addEventListener('click');
+chesenO.addEventListener('click');
+vsCPU.addEventListener('click');
+vsPlayer.addEventListener('click');
